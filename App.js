32ed1adeeadc12007dad0e39906d1e72fada715e
miillaadddddd <milad7212@gmail.com>
2021-09-15 20:41:13 +0430

    feat (card) : add productCard

--- App.js
+++ App.js
@@ -2,11 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
+      <Card
+        title="ساعت مچی "
+        subTitle="100$"
+        image={require("./app/assets/watch.jpg")}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
