a31fc873e8a257a2252ee3ff1cdc53688ddd6d0d
miillaadddddd <milad7212@gmail.com>
2021-09-19 11:10:03 +0430

    feat (beauty text)

--- app/components/AppText/AppText.jsx
+++ app/components/AppText/AppText.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Text } from "react-native";
 import styles from "./styles";
-
+import defaultStyles from "../../config/colors";
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
 
 export default AppText;
--- app/components/AppTextInput.jsx
+++ app/components/AppTextInput.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Platform, StyleSheet, TextInput, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import colors from "../config/colors";
+i;
+import defaultStyles from "../config/styles";
 
 function AppTextInput({ icon, ...otherProps }) {
   return (
@@ -10,28 +11,24 @@ function AppTextInput({ icon, ...otherProps }) {
         <MaterialCommunityIcons
           name={icon}
           size={20}
-          color={colors.medium}
+          color={defaultStyles.colors.medium}
           style={styles.icon}
         />
       )}
-      <TextInput style={styles.textInput} {...otherProps} />
+      <TextInput style={defaultStyles.text} {...otherProps} />
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: "row",
     width: "100%",
     padding: 15,
     marginVertical: 10,
   },
-  textInput: {
-    color: colors.dark,
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-  },
+
   icon: {
     marginRight: 10,
   },
