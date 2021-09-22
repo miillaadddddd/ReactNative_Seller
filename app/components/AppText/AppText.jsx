8bacd995c9d55c5d7535775b82d790095b48a1c2
miillaadddddd <milad7212@gmail.com>
2021-09-22 09:30:06 +0330

    8_3: Handling Long Text

--- app/components/AppText/AppText.jsx
+++ app/components/AppText/AppText.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import { Text } from "react-native";
 import styles from "./styles";
 import defaultStyles from "../../config/colors";
-function AppText({ children, style }) {
-  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
+function AppText({ children, style, ...otherProps }) {
+  return (
+    <Text style={[defaultStyles.text, style]} {...otherProps}>
+      {children}
+    </Text>
+  );
 }
 
 export default AppText;
