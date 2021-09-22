c8b5d5eff0db22a5b6e5e84e49f9d9169b038b5c
miillaadddddd <milad7212@gmail.com>
2021-09-22 09:25:10 +0330

    8_2 : Adding Chervons

--- app/components/ListItem.jsx
+++ app/components/ListItem.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import {
-  Image,
-  StyleSheet,
-  TouchableHighlight,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Image, StyleSheet, TouchableHighlight, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 import colors from "../config/colors";
 import AppText from "./AppText/AppText";
-import Swipeable from "react-native-gesture-handler/Swipeable";
 function ListItem({
   image,
   title,
@@ -27,6 +22,11 @@ function ListItem({
             <AppText style={styles.title}>{title}</AppText>
             {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
+          <MaterialCommunityIcons
+            color={colors.medium}
+            name="chevron-right"
+            size={25}
+          />
         </View>
       </TouchableHighlight>
     </Swipeable>
@@ -35,11 +35,13 @@ function ListItem({
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
     flexDirection: "row",
     padding: 15,
     backgroundColor: colors.white,
   },
   detailContainer: {
+    flex: 1,
     marginLeft: 10,
     justifyContent: "center",
   },
