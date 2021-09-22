e140fd78f243f90f4f05ed3a19766b0b1cc36f02
miillaadddddd <milad7212@gmail.com>
2021-09-22 07:40:41 +0330

    7_10: Organizing Components

--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 import { Image, StyleSheet } from "react-native";
-import { Formik } from "formik";
+
 import * as Yup from "yup";
-import AppButton from "../components/AppButton";
-import AppTextInput from "../components/AppTextInput";
+
 import Screen from "../components/Screen";
-import AppText from "../components/AppText/AppText";
-import ErrorMessage from "../components/ErrorMessage";
-import AppFormField from "../components/AppFormField";
-import SubmitButton from "../components/SubmitButton";
-import AppForm from "../components/AppForm";
+
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
