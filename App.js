fa39e0dc2366ca1c7bc0aec16e59f87db87a89a0
miillaadddddd <milad7212@gmail.com>
2021-09-26 16:58:20 +0330

    2_4 : Requesting Permissions

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import * as ImagePicker from "expo-image-picker";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) {
+      alert("You need to enable permission to access the library.");
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return <ListingsScreen></ListingsScreen>;
 }
