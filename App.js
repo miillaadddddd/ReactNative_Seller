2dc2c1593810c561aadf3baf5f9997d2764dfddc
miillaadddddd <milad7212@gmail.com>
2021-09-26 18:38:30 +0330

    2_6: Accessing the library

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import { Button } from "react-native";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState(initialState);
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
     if (!granted) {
@@ -14,6 +17,21 @@ export default function App() {
   useEffect(() => {
     requestPermission();
   }, []);
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
-  return <ListingsScreen></ListingsScreen>;
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
